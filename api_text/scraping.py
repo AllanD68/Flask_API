1116f244d3487fe88318baf671a6b5ca853bed70
Allan <allan.deoliveira@outlook.com>
2022-02-15 13:38:29 +0100

    ajout de la conversion en dictionnaire

--- api_text/scraping.py
+++ api_text/scraping.py
@@ -1,38 +1,41 @@
-from turtle import title
 import requests
 from bs4 import BeautifulSoup
+import networkx as nx
+from collections import Counter
+import matplotlib.pyplot as plt
+import pydot
+from networkx.drawing.nx_pydot import graphviz_layout
 
-urlvar = input("écris l'url  :")
+# urlvar = input("écris l'url  :")
 
-url = 'https://fr.wikipedia.org/wiki/' + urlvar
+# url = 'https://fr.wikipedia.org/wiki/' + urlvar
 
+url = 'https://fr.wikipedia.org/wiki/'
 
 response = requests.get(url)
 
 wikiP = []
 
 if response.ok:
+
     soup = BeautifulSoup(response.text , 'lxml')
-    # title = soup.find('title')
-    # print('title.text')
-    # wikiP = soup.find("tr", {"class": "ranking-list"})
     
     wikiWeb = soup.find("div", {"class":"mw-parser-output"}).findAll('p')
 
     wikiP = [ i.text for i in wikiWeb ]
 
+    scrapSplit = [ s.split() for s in  wikiP ]
 
-    ScrapWord = [ s.split() for s in  wikiP ]
+    scrapWord = [ item for sublist in scrapSplit for item in sublist] 
+
+    scrapCount = Counter(scrapWord)
+
+    print(scrapCount)
 
 
-
-    for n in range(len(ScrapWord)):
-        print(ScrapWord[n])
 else:
     print("l'url que t'as mis est pas juste ")
 
 
+# -------------------------------------------
 
-
-    # print(wikiP)
-    # di-ib clearfix
