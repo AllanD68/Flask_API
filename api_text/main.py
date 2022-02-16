687b10dcd14c274e86ecda970a4fb8b0133ab83a
Allan <allan.deoliveira@outlook.com>
2022-02-16 15:31:25 +0100

    ajout de la suppresion total

--- api_text/main.py
+++ api_text/main.py
@@ -1,5 +1,6 @@
 from email import message
-from flask import Flask, request
+from urllib import response
+from flask import Flask, jsonify, make_response, request
 from flask_restful import Api, Resource, abort
 from numpy import delete
 from utils.crud_scraping import wikiScraping
@@ -23,12 +24,29 @@ class Text(Resource):
 
 class All_text(Resource):
     def get(self):
-        return 'hello'
+        if len(os.listdir('JsonDB')) < 0:
+            dicRead = {}
+            for i in os.listdir('JsonDB'):
+                # Deleting the file.
+                os.remove(f"JsonDB/" + i)
+            response=make_response(jsonify({"message" : ""}))
+    
+    def delete(self):
+        if len(os.listdir('JsonDB')) > 0:
+            for i in os.listdir('JsonDB'):
+                # Deleting the file.
+                os.remove(f"JsonDB/" + i)
+            response=make_response(jsonify({"message" : "yolo t'as tout supp"}))
+            return response
+            
+        else:
+            abort (404,message="le dossier est vide")
+
 
 
 
 api.add_resource(Text,"/text/<test>") 
-api.add_resource(All_text,"/All_text/")
+api.add_resource(All_text,"/All_text")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- api_text/test.py
+++ api_text/test.py
@@ -2,10 +2,13 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# my_test=["france","isaac newton","jean claude vandamme"]
-# print(my_test[0])
-# for each in my_test:
-# response =requests.get(BASE +"text/"+my_test[0])
-# print(response.json())
-response =requests.delete(BASE +"text/france")
+# my_test=["naruto","dragon ball","one piece"]
+
+# for i in my_test:
+#     response =requests.get(BASE +"text/"+i)
+#     print(response.json())
+
+
+response =requests.delete(BASE +"All_text")
 print(response.json())
+
